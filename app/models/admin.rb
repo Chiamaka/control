7d99d45e619e9616c83e591c269d1494e2fd7689
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2015-11-27 08:25:42 +0000

    Add Bank Registration View

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,11 @@
 class Admin < ActiveRecord::Base
 	has_secure_password
+	
+	validates :username, presence: true
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true
+	validates :security_question, presence: true
+	validates :security_answer, presence: true
+	validates :phone, presence: true
+	validates :level, presence: true
 end
--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,4 +1,16 @@
 class Bank < ActiveRecord::Base
 	has_many :accounts
 	has_many :customers, class_name: 'Citizen', through: :accounts
+
+	validates :name, presence: true, uniqueness: true
+	#validates :bank_type, presence: true
+	validates :reg_no, presence: true, uniqueness: true
+	validates :founded, presence: true
+	validates :headquarters, presence: true
+	validates :ceo, presence: true
+	validates :services, presence: true
+	validates :email, presence: true
+	validates :website, presence: true
+	
+
 end
